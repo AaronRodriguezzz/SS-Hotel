acb198931c5c9000083634da21646837980ed6e8
lindsey5 <lindseysamson5@gmail.com>
2024-12-09 01:13:27 +0800

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -14,15 +14,14 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 
-app.use(express.json());       
-app.use(cookieParser());   
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
 app.use(cors({
     origin: 'http://localhost:5173', 
     credentials: true, 
 }));
-
+app.use(express.json());       
+app.use(cookieParser());   
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.use((req,res,next) => {
     console.log(req.path, req.method);
